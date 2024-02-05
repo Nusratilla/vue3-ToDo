fed4673ffae5d7a9452b9d50996ed6cf2dfa97dc
Nusrat <nusratilla.88@gmail.com>
2024-02-05 23:56:16 +0500

    btn

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,29 @@
         Hello! <input type="text" placeholder="Name Here" v-model="name">
       </h2>
     </section>
+
+    <section class="create-todo">
+      <h3>Create A ToDo</h3>
+      <form @submit.prevent="addTodo">
+        <h4>Whats's on your todo list?</h4>
+        <input type="text" placeholder="e.g. Study IT" v-model="input_content">
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" value="business" v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input type="radio" name="category" value="personal" v-model="input_category" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+      </form>
+    </section>
+
+
   </main>
 </template>
 
