96c0a217870bb92eb7d0e6b752501e6cf710d3c8
Nusrat <nusratilla.88@gmail.com>
2024-02-05 23:25:28 +0500

    localstorage added name

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 
 
 <template>
-  <div>
-    <h2>Hello World</h2>
-  </div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Hello! <input type="text" placeholder="Name Here" v-model="name">
+      </h2>
+    </section>
+  </main>
 </template>
 
 <script setup>
+import { ref, onMounted, computed, watch } from 'vue'
 
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return a.createdAt - b.createdAt
+}))
+
+watch(name, (newValue) => {
+  localStorage.setItem('name', newValue)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
 </script>
 
-<style scoped></style>
